4bcb0fe68b704879f7b2044a104e07c82f75ef3b
hallyl <gentlekid12@gmail.com>
2019-07-23 14:05:03 +0000

    day 2

---
+++ auth.js
@@ -0,0 +1,17 @@
+(function(){
+var firebaseConfig = {
+			apiKey: "AIzaSyDKemERNtJH-j2iPFmxXDc-Xw7m9k1omTQ",
+			authDomain: "adtech-84995.firebaseapp.com",
+			databaseURL: "https://adtech-84995.firebaseio.com",
+			projectId: "adtech-84995",
+			storageBucket:"",
+			messagingSenderId: "795847057867",
+			appId: "1:795847057867:web:35fc3466642e1738"
+		};
+		
+		firebase.initializeApp(firebaseConfig);
+		
+		app_fireBase = firebase;
+		
+		app_fireBase.prototype.setData = function
+})()
